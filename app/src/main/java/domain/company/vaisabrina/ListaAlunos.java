703229f79c955ddb184fd93167dc29b06a2a085e
Emiliano Barbosa <emilianoeloi@gmail.com>
2017-07-17 14:45:10 -0300

    CHANGE: incluindo clique nos itens

--- app/src/main/java/domain/company/vaisabrina/ListaAlunos.java
+++ app/src/main/java/domain/company/vaisabrina/ListaAlunos.java
@@ -3,8 +3,11 @@ package domain.company.vaisabrina;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by android7045 on 17/07/17.
@@ -22,5 +25,22 @@ public class ListaAlunos extends Activity {
         this.listaAlunos = (ListView)findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
+
+        listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ListaAlunos.this, "Posicao Selecionada: " + i, Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        listaAlunos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = listaAlunos.getItemAtPosition(i).toString();
+                Toast.makeText(ListaAlunos.this, "Nome Selecionado "+name, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 }
