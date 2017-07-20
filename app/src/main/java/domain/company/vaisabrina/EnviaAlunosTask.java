404397cb3f4306129e134fd223545eacfbdfdbad
Emiliano Barbosa <emilianoeloi@gmail.com>
2017-07-20 15:29:09 -0300

    CHANGE: resultado do envio das notas

--- app/src/main/java/domain/company/vaisabrina/EnviaAlunosTask.java
+++ app/src/main/java/domain/company/vaisabrina/EnviaAlunosTask.java
@@ -1,9 +1,14 @@
 package domain.company.vaisabrina;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -42,6 +47,22 @@ public class EnviaAlunosTask extends AsyncTask<Object, Object, String> {
         super.onPostExecute(s);
         progressDialog.dismiss();
         Toast.makeText(context, "On Post Execute "+s, Toast.LENGTH_LONG).show();
+        final Dialog dialog = new Dialog(context); // Context, this, etc.
+        dialog.setContentView(R.layout.dialog_resultado_nota);
+        dialog.setTitle("Resultado");
+        TextView dialogMedia = (TextView)dialog.findViewById(R.id.dialog_media);
+        TextView dialogQuantidade = (TextView)dialog.findViewById(R.id.dialog_quantidade);
+        try {
+            JSONObject resultado = new JSONObject(s);
+            String media = resultado.getString("media");
+            String quantidade = resultado.getString("quantidade");
+            dialogMedia.setText(media);
+            dialogQuantidade.setText(quantidade);
+        } catch(JSONException e) {
+
+        }
+
+        dialog.show();
 
     }
 }
