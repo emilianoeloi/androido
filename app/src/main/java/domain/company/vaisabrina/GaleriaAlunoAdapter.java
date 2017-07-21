931ecc31e48b6467ae194b3fa320da05ac47b247
Emiliano Barbosa <emilianoeloi@gmail.com>
2017-07-21 14:07:39 -0300

    CHANGE: view pager

--- app/src/main/java/domain/company/vaisabrina/GaleriaAlunoAdapter.java
+++ app/src/main/java/domain/company/vaisabrina/GaleriaAlunoAdapter.java
@@ -36,7 +36,7 @@ public class GaleriaAlunoAdapter extends PagerAdapter {
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        super.destroyItem(container, position, object);
+//        super.destroyItem(container, position, object);
         ((ViewPager) container).removeView((View)object);
     }
 
